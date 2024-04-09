737d883a3769dd3f09196ac761c800a41c90b1d4
Clancy76 <monumehta2702@gmail.com>
2024-04-10 00:54:50 +0530

    fifth commit

--- src/utils/baseUrl.js
+++ src/utils/baseUrl.js
@@ -1,4 +1,4 @@
 export const baseUrl =
   process.env.NODE_ENV === "production"
-    ? "https://next-pizza-i3tfybd1d-monu-kumars-projects.vercel.app/"
+    ? process.env.NEXT_PUBLIC_BASE_URL
     : "http://localhost:3000/";
