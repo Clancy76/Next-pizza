6cff16e66c50de2027edfa6ccd1126b98a9581e0
Clancy76 <monumehta2702@gmail.com>
2024-04-13 20:44:44 +0530

    change in utils

--- src/utils/baseUrl.js
+++ src/utils/baseUrl.js
@@ -1,4 +1,4 @@
 export const baseUrl =
   process.env.NODE_ENV === "production"
-    ? "https://next-pizza-ofyczxgr8-monu-kumars-projects.vercel.app/"
+    ? process.env.NEXT_PUBLIC_BASE_URL
     : "http://localhost:3000/";
