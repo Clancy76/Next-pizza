cf16e54927ec37847b7378e5a7cfac1c55e43093
Clancy76 <monumehta2702@gmail.com>
2024-06-15 13:14:03 +0530

    carousel fix

--- src/components/home/Carousel.js
+++ src/components/home/Carousel.js
@@ -22,7 +22,7 @@ function CarouselComponent() {
             className="object-center brightness-50"
           >
             <img
-              src={`https://source.unsplash.com/random/900x600/?${image}`}
+              src={"https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.thepioneerwoman.com%2Ffood-cooking%2Frecipes%2Fa36080074%2Fclassic-cheese-pizza%2F&psig=AOvVaw2ym2d3w-bgH4a_yP_9o4Gx&ust=1718523759014000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCOC9vJOO3YYDFQAAAAAdAAAAABAE"}
               alt="pizza"
             />
           </div>
