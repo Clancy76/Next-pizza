df80501433aff4a62b8a6735344073f4a207f758
Clancy76 <monumehta2702@gmail.com>
2024-06-15 13:29:53 +0530

    carousel fix

--- src/components/home/Carousel.js
+++ src/components/home/Carousel.js
@@ -22,7 +22,11 @@ function CarouselComponent() {
             className="object-center brightness-50"
           >
             <img
-              src={"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSJYqXs_eYTVeNtugtP2q7zV_ROewS7r5-Gkg&s"}
+              src={"https://i.pinimg.com/originals/ae/64/fd/ae64fd89dec81365bd9230736b292828.jpg"}
+              alt="pizza"
+            />
+            <img
+              src={"https://static.vecteezy.com/system/resources/previews/030/660/303/large_2x/pizza-with-white-background-high-quality-ultra-hd-free-photo.jpg"}
               alt="pizza"
             />
           </div>
